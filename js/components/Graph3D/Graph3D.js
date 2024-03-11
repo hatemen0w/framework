dc94f0a0c731b1f6ddd06cd3194de61663d3e0e9
hatemen0w <denisred2005@gmail.com>
2024-03-11 22:07:25 +0400

    2

--- js/components/Graph3D/Graph3D.js
+++ js/components/Graph3D/Graph3D.js
@@ -1,61 +1,124 @@
 class Graph3D extends Component {
     constructor(options) {
         super(options);
-        const WIN = {
+        this.WIN = {
             LEFT: -10,
             BOTTOM: -10,
             WIDTH: 20,
             HEIGHT: 20,
             CENTER: new Point(0, 0, -30),
             CAMERA: new Point(0, 0, -50)
-        }
-        this.graph = new Graph({ id: 'canvasGraph3D', width: 600, height: 600, WIN });
-        this.math3D = new Math3D({ WIN });
+        };
+
+        this.controls = {
+            showPoints: true,
+            showEdges: true,
+            showPolygons: true,
+        };
+
+        this.graph = new Graph({
+            id: 'canvasGraph3D', width: 600, height: 600, WIN: this.WIN,
+            callbacks: {
+                wheel: (event) => this.wheel(event),
+                mousemove: (event) => this.mousemove(event),
+                mouseup: () => this.mouseup(),
+                mousedown: () => this.mousedown(),
+            }
+        });
+
+        this.math3D = new Math3D({ WIN: this.WIN });
         const surfaces = new Surfaces;
         this.scene = surfaces.ellipse();
+        this.initUI();
         this.renderScene();
     }
 
+    initUI() {
+        const controlsDiv = document.createElement('div');
+        controlsDiv.innerHTML = `
+            <label><input type="checkbox" id="showPoints" checked> Показывать точки</label>
+            <label><input type="checkbox" id="showEdges" checked> Показывать рёбра</label>
+            <label><input type="checkbox" id="showPolygons" checked> Показывать полигоны</label>
+        `;
+    
+        if (this.graph.container) {
+            this.graph.container.appendChild(controlsDiv);
+        } else {
+            document.body.appendChild(controlsDiv);
+        }
+    
+        controlsDiv.addEventListener('change', (event) => {
+            const checkbox = event.target;
+            const controlId = checkbox.id;
+    
+            if (controlId in this.controls) {
+                this.controls[controlId] = checkbox.checked;
+                this.graph.clear();
+                this.renderScene();
+            }
+        });
+    }
 
     renderScene() {
-        this.scene.points.forEach(point => this.graph.point(this.math3D.xs(point), this.math3D.ys(point)));
-        this.scene.edges.forEach(edge => {
-            const point1 = this.scene.points[edge.p1];
-            const point2 = this.scene.points[edge.p2];
-            this.graph.line(this.math3D.xs(point1), this.math3D.ys(point1), this.math3D.xs(point2), this.math3D.ys(point2));
+        if (this.controls.showPoints) {
+            this.scene.points.forEach(point => this.graph.point(this.math3D.xs(point), this.math3D.ys(point)));
+        }
+
+        if (this.controls.showEdges) {
+            this.scene.edges.forEach(edge => {
+                const point1 = this.scene.points[edge.p1];
+                const point2 = this.scene.points[edge.p2];
+                this.graph.line(this.math3D.xs(point1), this.math3D.ys(point1), this.math3D.xs(point2), this.math3D.ys(point2));
+            });
+        }
+
+        if (this.controls.showPolygons) {
+            this.math3D.calcDistance(this.scene, this.WIN.CAMERA, 'distance');
+            this.math3D.sortByArtistAlgorithm(this.scene);
+            this.drawPolygons();
+        }
+    }
+
+    addPolygons(polygons) {
+        polygons.forEach(polygon => this.scene.polygons.push(polygon));
+        this.graph.clear();
+        this.renderScene();
+    }
+
+    drawPolygons() {
+        this.scene.polygons.forEach(polygon => {
+            const points = polygon.points.map(index => new Point(this.scene.points[index]));
+            this.graph.polygon(points, polygon.color);
         });
-    } 
+    }
 
-    // callbacks: {
-    //     wheel: (event) => this.wheel(event),
-    //     mousemove: (event) => this.mousemove(event),
-    //     mouseup: (event) => this.mouseup(),
-    //     mousedown: (event) => this.mousedown(),
-    // }
+    mouseup() {
+        this.canMove = false;
+    }
 
-    // wheel(event) {
-    //     event.parentDefault();
-    //     const delta = (event.wheelDelta > 0) ? -0.3 : 0.3;
-    //     this.scene.points.foeEach(point => this.math3D.zoom(point, delta));
-    //     this.renderScene();
-    // }
+    mousedown() {
+        this.canMove = true;
+    }
 
-    // mousemove(event) {
-    //     if (this.canMove) {
-    //         const gradus = Math.PI / 180 / 4;
-    
-    //         this.renderScene.points.forEach(point => {
-    //             this.math3D.rotate0x(point, (this.dy - event.offsetY) * gradus);
-    //             this.math3D.rotate0y(point, (this.dx - event.offsetX) * gradus);
-    //         });
-    //     }
-    // }
+    wheel(event) {
+        event.preventDefault();
+        const delta = (event.wheelDelta > 0) ? 1.1 : 0.9;
+        this.scene.points.forEach(point => this.math3D.zoom(delta, point));
+        this.graph.clear();
+        this.renderScene();
+    }
 
-    // mouseup(){
-    //     this.canMove=falese;
-    // }
-
-    // mousedown(){
-    //     this.canMove=true;
-    // }
+    mousemove(event) {
+        if (this.canMove) {
+            const gradus = Math.PI / 180 / 4;
+            this.scene.points.forEach(point => {
+                this.math3D.rotateOx(point, (this.dy - event.offsetY) * gradus);
+                this.math3D.rotateOy(point, (this.dx - event.offsetX) * gradus);
+            });
+            this.graph.clear();
+            this.renderScene();
+        }
+        this.dx = event.offsetX;
+        this.dy = event.offsetY;
+    }
 }
--- js/modules/Math3D/Math3D.js
+++ js/modules/Math3D/Math3D.js
@@ -15,11 +15,11 @@ class Math3D {
         const zs = this.WIN.CENTER.z;
         const z0 = this.WIN.CAMERA.z;
         const y0 = this.WIN.CAMERA.y;
-        return -(point.y - y0) / (point.z - z0) * (zs - z0) + y0;
+        return (point.y - y0) / (point.z - z0) * (zs - z0) + y0;
     }
 
     multMatrix(T, m) {
-        const a = [0, 0, 0, 0]
+        const a = [0, 0, 0, 0];
         for (let i = 0; i < T.length; i++) {
             let b = 0;
             for (let j = 0; j < m.length; j++) {
@@ -30,22 +30,87 @@ class Math3D {
         return a;
     }
 
-    zoom(point,delta){
+    zoom(delta, point) {
         const T = [
-            [delta,0,0,0]
-            [0,delta,0,0]
-            [0,0,delta,0]
-            [0,0,0,1]
+            [delta, 0, 0, 0],
+            [0, delta, 0, 0],
+            [0, 0, delta, 0],
+            [0, 0, 0, 1]
         ];
-        const array = this.multMatrix(T,[point.x,point.y,point.z,1])
-
-        point.x=array[0]
-        point.y=array[1]
-        point.z=array[2]
-
+        const array = this.multMatrix(T, [point.x, point.y, point.z, 1]);
+        point.x = array[0];
+        point.y = array[1];
+        point.z = array[2];
     }
 
-    move(point, dx=0,dy=0,dz=0){
+    move(point, dx = 0, dy = 0, dz = 0) {
+        const T = [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [dx, dy, dz, 1]
+        ]
+        const array = this.multMatrix(T, [point.x, point.y, point.z, 1]);
+        point.x = array[0];
+        point.y = array[1];
+        point.z = array[2];
+    }
 
+    rotateOx(point, alpha) {
+        const T = [
+            [1, 0, 0, 0],
+            [0, Math.cos(alpha), Math.sin(alpha), 0],
+            [0, -Math.sin(alpha), Math.cos(alpha), 0],
+            [0, 0, 0, 1],
+        ]
+        const array = this.multMatrix(T, [point.x, point.y, point.z, 1]);
+        point.x = array[0];
+        point.y = array[1];
+        point.z = array[2];
+    }
+
+    rotateOy(point, alpha) {
+        const T = [
+            [Math.cos(alpha), 0, -Math.sin(alpha), 0],
+            [0, 1, 0, 0],
+            [Math.sin(alpha), 0, Math.cos(alpha), 0],
+            [0, 0, 0, 1],
+        ]
+        const array = this.multMatrix(T, [point.x, point.y, point.z, 1]);
+        point.x = array[0];
+        point.y = array[1];
+        point.z = array[2];
+    }
+
+    rotateOz(point, alpha) {
+        const T = [
+            [Math.cos(alpha), Math.sin(alpha), 0, 0],
+            [-Math.sin(alpha), Math.cos(alpha), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ]
+        const array = this.multMatrix(T, [point.x, point.y, point.z, 1]);
+        point.x = array[0];
+        point.y = array[1];
+        point.z = array[2];
+    }
+
+    calcDistance(surface, endPoint, name) {
+        surface.polygons.forEach(polygon => {
+            let x = 0, y = 0, z = 0;
+            polygon.points.forEach(index => {
+                x += surface.points[index].x;
+                y += surface.points[index].y;
+                z += surface.points[index].z;
+            });
+            x /= polygon.points.length;
+            y /= polygon.points.length;
+            z /= polygon.points.length;
+            polygon[name] = math.sqrt(endPoint.x - x) ** 2 + (endPoint.y - y) ** 2 + (endPoint.z - z) ** 2;
+        })
+    }
+
+    sortByArtistAlgorithm(surface){
+        surface.polygons.sort((a,b)=>(a.distance<b.distance)?1:-1);
     }
 }
